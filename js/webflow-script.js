082b270d4c29279bf908b0f4a3e3ab10c13cb114
penelopeazuelos <penelope.azuelos@gmail.com>
2025-11-05 21:53:35 +0000

    Update webflow-script.js

--- js/webflow-script.js
+++ js/webflow-script.js
@@ -562,8 +562,7 @@ t[a]||(i===E.EventTypeConsts.MOUSE_CLICK||i===E.EventTypeConsts.MOUSE_SECOND_CLI
                         ]
                     }
                 ],
-                createdOn:0x199b07902f1,
-                useFirstGroupAsInitialState:!1
+                createdOn:0x199b07902f1,useFirstGroupAsInitialState:!1
             },
             "a-7":
             {
