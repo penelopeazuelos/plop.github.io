ea316e176a27743b381e1ebd6b34cdc88e6373f8
penelopeazuelos <penelope.azuelos@gmail.com>
2025-11-06 19:23:28 +0000

    Update webflow-script.js

--- js/webflow-script.js
+++ js/webflow-script.js
@@ -5534,77 +5534,68 @@
               createdOn: 0x199b51fe64a
             },
             "e-21": {
-              id: "e-21",
-              name: "",
-              animationType: "custom",
-              eventTypeId: "MOUSE_CLICK",
-              action: {
-                id: "",
-                actionTypeId: "GENERAL_START_ACTION",
-                config: {
-                  delay: 0,
-                  easing: "",
-                  duration: 0,
-                  actionListId: "a-7",
-                  affectedElements: {},
-                  playInReverse: !1,
-                  autoStopEventId: "e-22"
-                }
-              },
-              mediaQueries: ["main", "medium", "small", "tiny"],
-              target: {
-                appliesTo: "ELEMENT",
-                styleBlockIds: [],
-                id: "66ccf03f7ceb58d801f12c9e|incomplete_real_trees"
-              },
-              targets: [],
-              config: {
-                loop: !1,
-                playInReverse: !1,
-                scrollOffsetValue: null,
-                scrollOffsetUnit: null,
-                delay: null,
-                direction: null,
-                effectIn: null
-              },
-              createdOn: 0x199b51fe64a
-            },
-            "e-22": {
-              id: "e-22",
-              name: "",
-              animationType: "custom",
-              eventTypeId: "MOUSE_SECOND_CLICK",
-              action: {
-                id: "",
-                actionTypeId: "GENERAL_START_ACTION",
-                config: {
-                  delay: 0,
-                  easing: "",
-                  duration: 0,
-                  actionListId: "a-8",
-                  affectedElements: {},
-                  playInReverse: !1,
+                id: "e-21",
+                name: "Show Abstract 4",
+                animationType: "custom",
+                eventTypeId: "MOUSE_CLICK",
+                action: {
+                    id: "",
+                    actionTypeId: "GENERAL_START_ACTION",
+                    config: {
+                      delay: 0,
+                      easing: "",
+                      duration: 0,
+                      actionListId: "a-7",
+                      affectedElements: {
+                        "66ccf03f7ceb58d801f12c9e|ab4": {
+                          id: "66ccf03f7ceb58d801f12c9e|ab4",
+                          selector: null,
+                          selectorGuids: []
+                        }
+                      },
+                      playInReverse: false,
+                      autoStopEventId: "e-22"
+                    }
+                },
+                mediaQueries: ["main", "medium", "small", "tiny"],
+                target: {
+                  appliesTo: "ELEMENT",
+                  id: "66ccf03f7ceb58d801f12c9e|incomplete_real_trees"
+                },
+                config: { loop: false }
+                },
+
+                "e-22": {
+                  id: "e-22",
+                  name: "Hide Abstract 4",
+                  animationType: "custom",
+                  eventTypeId: "MOUSE_SECOND_CLICK",
+                  action: {
+                    id: "",
+                    actionTypeId: "GENERAL_START_ACTION",
+                    config: {
+                      delay: 0,
+                      easing: "",
+                      duration: 0,
+                      actionListId: "a-8",
+                      affectedElements: {
+                        "66ccf03f7ceb58d801f12c9e|ab4": {
+                          id: "66ccf03f7ceb58d801f12c9e|ab4",
+                          selector: null,
+                          selectorGuids: []
+                        }
+                      },
+                      playInReverse: false,
                   autoStopEventId: "e-21"
+                    }
+                  },
+                  mediaQueries: ["main", "medium", "small", "tiny"],
+                  target: {
+                    appliesTo: "ELEMENT",
+                    id: "66ccf03f7ceb58d801f12c9e|incomplete_real_trees"
+                  },
+                  config: { loop: false }
                 }
-              },
-              mediaQueries: ["main", "medium", "small", "tiny"],
-              target: {
-                appliesTo: "ELEMENT",
-                styleBlockIds: [],
-                id: "66ccf03f7ceb58d801f12c9e|incomplete_real_trees"
-              },
-              targets: [],
-              config: {
-                loop: !1,
-                playInReverse: !1,
-                scrollOffsetValue: null,
-                scrollOffsetUnit: null,
-                delay: null,
-                direction: null,
-                effectIn: null
-              },
-              createdOn: 0x199b51fe64a
-            }
           },
           actionLists: {
             a: {
@@ -5776,61 +5767,56 @@
               useFirstGroupAsInitialState: !1
             },
             "a-7": {
-              id: "a-7",
-              title: "Abstract 4",
-              actionItemGroups: [{
-                actionItems: [{
-                  id: "a-7-n",
-                  actionTypeId: "GENERAL_DISPLAY",
-                  config: {
-                    delay: 0,
-                    easing: "",
-                    duration: 0,
-                    value: "none",
-                    target: {
-                      id: "66ccf03f7ceb58d801f12c9e|ab4"
+                id: "a-7",
+                title: "Show Abstract 4",
+                actionItemGroups: [
+                    {
+                    actionItems: [
+                    {
+                      id: "a-7-show",
+                      actionTypeId: "GENERAL_DISPLAY",
+                        config: {
+                            delay: 0,
+                            easing: "",
+                            duration: 0,
+                            value: "block",
+                            target: {
+                                id: "66ccf03f7ceb58d801f12c9e|ab4"
+                            }
+                        }
                     }
-                  }
-                }]
-              }, {
-                actionItems: [{
-                  id: "a-7-n-2",
-                  actionTypeId: "GENERAL_DISPLAY",
-                  config: {
-                    delay: 0,
-                    easing: "",
-                    duration: 0,
-                    value: "block",
-                    target: {
-                      id: "66ccf03f7ceb58d801f12c9e|ab4"
+                    ]
                     }
-                  }
-                }]
-              }],
-              createdOn: 0x199b07860e0,
-              useFirstGroupAsInitialState: !0
-            },
-            "a-8": {
-              id: "a-8",
-              title: "Hide 4",
-              actionItemGroups: [{
-                actionItems: [{
-                  id: "a-8-n",
-                  actionTypeId: "GENERAL_DISPLAY",
-                  config: {
-                    delay: 0,
-                    easing: "",
-                    duration: 0,
-                    value: "none",
-                    target: {
-                      id: "66ccf03f7ceb58d801f12c9e|ab4"
+                ],
+                createdOn: Date.now(),
+                useFirstGroupAsInitialState: false
+                },
+
+                "a-8": {
+                    id: "a-8",
+                    title: "Hide Abstract 4",
+                    actionItemGroups: [
+                    {
+                    actionItems: [
+                        {
+                        id: "a-8-hide",
+                        actionTypeId: "GENERAL_DISPLAY",
+                        config: {
+                            delay: 0,
+                            easing: "",
+                            duration: 0,
+                            value: "none",
+                            target: {
+                              id: "66ccf03f7ceb58d801f12c9e|ab4"
+                            }
+                        }
+                        }
+                      ]
                     }
-                  }
-                }]
-              }],
-              createdOn: 0x199b07902f1,
-              useFirstGroupAsInitialState: !1
-            },
+                  ],
+                  createdOn: Date.now(),
+                  useFirstGroupAsInitialState: false
+                },
             pop: {
               id: "pop",
               actionItemGroups: [{
